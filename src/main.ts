1bdba5a93b9a65b886769d2e82d8083ecda91a85
ash <ash@asboy2035.com>
2026-01-13 13:44:41 +0100

    add PWA support

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { createHead } from '@unhead/vue/client'
 import App from '@/App.vue'
 import router from '@/router'
 import '@/styles/global.sass'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+})
 
 const bygWeb = createApp(App)
 const head = createHead()
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Byg Platform',
+        short_name: 'Byg',
+        description: 'Byg Platform for Web',
+        theme_color: '#69ffff',
+        background_color: '#557d8e',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logos/BygLogo-NoRound.png',
+            sizes: '1024x1024',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
